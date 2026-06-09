a815df53d5ef3f0a97638770388cdfcac93ae12d
berezinav <navnoire@gmail.com>
2026-06-09 23:20:31 +0300

    sprint_7: Marketplace app accessibility fix

--- app/src/main/java/ru/yandexpraktikum/marketplace/ui/theme/Theme.kt
+++ app/src/main/java/ru/yandexpraktikum/marketplace/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package ru.yandexpraktikum.marketplace.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
